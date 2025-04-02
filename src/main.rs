600887750efae9c740b8ecdf4064448397182ae7
Elvis Antti <elvis.antti@gmail.com>
2025-04-02 20:22:52 +0200

    Fixed

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,14 @@ fn main() {
     let data = include_bytes!("../font.ttf");
     let font = Font::try_from_bytes(data).expect("Failed to parse font");
 
-    widget.create_surface("status_bar".into());
+    widget.create_surface("status_bar".into()).unwrap();
 
 
     
     let mut time = chrono::Local::now().checked_sub_signed(TimeDelta::minutes(2)).unwrap();
     let mut wrk = -1;
+
+    let mut comp = widget.get_comp().unwrap();
 
 
     loop {
@@ -34,14 +36,15 @@ fn main() {
         time = chrono::Local::now();
         wrk = Workspace::get_active().unwrap().id - 1;
         let text = format!("{} --- {}",time.format("%H:%M, %d %b %Y"), wrk_icons.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(" "));
-        widget.draw_rect(0x7F000000.into(), Vector2::new(0.0,0.0), Vector2::new(1920.0, 30.0)).unwrap();
-        widget.draw_text(text.to_string(), Vector2::new(0.0,25.0), 30.0, font.clone(), |_,c| {
+        comp.draw_rect(0x7F000000.into(), Vector2::new(0.0,0.0), Vector2::new(1920.0, 30.0)).unwrap();
+        comp.draw_text(text.to_string(), Vector2::new(0.0,25.0), 30.0, font.clone(), |_,c| {
             if c == wrk_icons[wrk as usize] {
                 return 0xFF00FFFF.into()
             }
             0xFFFFFFFF.into()
         }, 0x7F000000.into()).unwrap();
 
-        widget.update_blocking().unwrap();
+        println!("Updating! {}", text);
+        comp.update_blocking().unwrap();
     }
 }
